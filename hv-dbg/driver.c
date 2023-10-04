4b0f8315f9b24ea03152ca6eb68429ab21358005
lhodges1 <lachiehodges7785@gmail.com>
2023-10-05 02:50:38 +1100

    bed time c

--- hv-dbg/driver.c
+++ hv-dbg/driver.c
@@ -1,18 +1,142 @@
 #include "driver.h"
 
+#include <intrin.h>
+
 UNICODE_STRING device_name = RTL_CONSTANT_STRING(L"hv-dbg");
 UNICODE_STRING device_link = RTL_CONSTANT_STRING(L"hv-dbg-link");
 
+typedef struct _VMM_STATE
+{
+        UINT64 vmxon_region_va;
+        UINT64 vmcs_region_va;
+        UINT64 vmxon_region_pa;
+        UINT64 vmcs_region_pa;
 
+}VMM_STATE, *PVMM_STATE;
 
+PVMM_STATE vmm_state = NULL;
+
+/*
+* VMCS region comprises up to 4096 bytes, with the following format:
+* 
+* offset 0: VMCS revision identifier
+* offset 4: VMX abort indicator
+* offset 8: VMCS data
+* 
+* Source: 3c 24.2
+*/
+STATIC
+BOOLEAN
+hvdbgAllocateVmcsRegion(
+        _In_ ULONG CoreNumber
+)
+{
+        PVOID virtual_allocation = NULL;
+        PVOID physical_allocation = NULL;
+        PHYSICAL_ADDRESS physical_max = { 0 };
+        PHYSICAL_ADDRESS physical_address = { 0 };
+        IA32_VMX_BASIC_MSR ia32_basic_msr = { 0 };
+
+        if (!CoreNumber)
+                return FALSE;
+
+        physical_max.QuadPart = MAXULONG64;
+
+        virtual_allocation = MmAllocateContiguousMemory(
+                ALIGNMENT_PAGE_SIZE,
+                physical_max
+        );
+
+        if (!virtual_allocation)
+                return FALSE;
+
+        RtlSecureZeroMemory(virtual_allocation, ALIGNMENT_PAGE_SIZE);
+
+        physical_address = MmGetPhysicalAddress(virtual_allocation);
+
+        if (!physical_address.QuadPart)
+        {
+                MmFreeContiguousMemory(virtual_allocation);
+                return FALSE;
+        }
+
+        physical_allocation = (PVOID)physical_address.QuadPart;
+
+        ia32_basic_msr.bit_address = __readmsr(MSR_IA32_VMX_BASIC);
+
+        *(UINT32*)virtual_allocation = ia32_basic_msr.bits.RevisionIdentifier;
+
+        if (__vmx_vmptrld(&physical_allocation))
+                return FALSE;
+
+        vmm_state[CoreNumber].vmxon_region_pa = (UINT64)physical_allocation;
+        vmm_state[CoreNumber].vmxon_region_va = (UINT64)virtual_allocation;
+
+        return TRUE;
+}
+
+STATIC
+BOOLEAN
+hvdbgAllocateVmxonRegion(
+        _In_ CoreNumber
+)
+{
+        PVOID virtual_allocation = NULL;
+        PVOID physical_allocation = NULL;
+        PHYSICAL_ADDRESS physical_max = { 0 };
+        PHYSICAL_ADDRESS physical_address = { 0 };
+        IA32_VMX_BASIC_MSR ia32_basic_msr = { 0 };
+
+        if (!CoreNumber)
+                return FALSE;
+
+        physical_max.QuadPart = MAXULONG64;
+
+        virtual_allocation = MmAllocateContiguousMemory(
+                ALIGNMENT_PAGE_SIZE,
+                physical_max
+        );
+
+        if (!virtual_allocation)
+                return FALSE;
+
+        RtlSecureZeroMemory(virtual_allocation, ALIGNMENT_PAGE_SIZE);
+
+        physical_address = MmGetPhysicalAddress(virtual_allocation);
+
+        if (!physical_address.QuadPart)
+        {
+                MmFreeContiguousMemory(virtual_allocation);
+                return FALSE;
+        }
+
+        physical_allocation = (PVOID)physical_address.QuadPart;
+
+        ia32_basic_msr.bit_address = __readmsr(MSR_IA32_VMX_BASIC);
+
+        *(UINT32*)virtual_allocation = ia32_basic_msr.bits.RevisionIdentifier;
+
+        if (__vmx_on(&physical_allocation))
+                return FALSE;
+
+        vmm_state[CoreNumber].vmcs_region_pa = (UINT64)physical_allocation;
+        vmm_state[CoreNumber].vmcs_region_va = (UINT64)virtual_allocation;
+
+        return TRUE;
+}
+
+/*
+* Here we check for vmx support by checking the CPUID.1:ECX.VMX[bit 5]
+* https://en.wikipedia.org/wiki/CPUID#EAX=1:_Processor_Info_and_Feature_Bits
+* Source: 3c 23.6
+* 
+* We then enable the use of VMXON inside and outside of SMX. 
+* Source: 3c 23.7
+*/
 STATIC
 BOOLEAN
 hvdbgIsVmxSupported()
 {
-        //Here we check for vmx support by checking the CPUID.1:ECX.VMX[bit 5]
-        //https://en.wikipedia.org/wiki/CPUID#EAX=1:_Processor_Info_and_Feature_Bits
-        //source: 3c 23.6
-
         CPUID cpuid = { 0 };
         IA32_FEATURE_CONTROL_MSR control_msr = { 0 };
 
@@ -21,24 +145,71 @@ hvdbgIsVmxSupported()
         if ((cpuid.ecx & (1 << 5)) == FALSE)
                 return FALSE;
 
-        //Here we enable the use of VMXON inside and outside of SMX
-        //Bit 0 is the lockbit, if its set to 0 VMXON will cause an exception
-        //Bit 2 enables VMXON outside of SMX operation
-        //source: 3c 23.7
-
-        control_msr.All = __readmsr(MSR_IA32_FEATURE_CONTROL);
+        control_msr.bit_address = __readmsr(MSR_IA32_FEATURE_CONTROL);
 
         if (control_msr.bits.Lock == FALSE)
         {
                 control_msr.bits.Lock = TRUE;
                 control_msr.bits.EnableVmxon = TRUE;
 
-                __writemsr(MSR_IA32_FEATURE_CONTROL, control_msr.All);
+                __writemsr(MSR_IA32_FEATURE_CONTROL, control_msr.bit_address);
         }
         else if (control_msr.bits.EnableVmxon == FALSE)
         {
                 DEBUG_LOG("VMX locked off in BIOS");
                 return FALSE;
+        }
+
+        return TRUE;
+}
+
+/*
+* Assuming the thread calling this is binded to a particular core
+*/
+STATIC
+VOID
+hvdbgEnableVmxOperationOnCore()
+{
+        CR4 cr4 = { 0 };
+        cr4.bit_address = __readcr4();
+        cr4.bits.vmxe = TRUE;
+        __writecr4(cr4.bit_address);
+}
+
+/*
+* Initialise VMX operation on each logical core
+*/
+STATIC
+BOOLEAN
+hvdbgInitiateVmxOperation()
+{
+        ULONG processor_count = 0;
+
+        if (!hvdbgIsVmxSupported())
+                return FALSE;
+
+        processor_count = KeQueryActiveProcessorCount(0);
+
+        if (!processor_count)
+                return FALSE;
+
+        vmm_state = ExAllocatePoolWithTag(
+                POOL_FLAG_NON_PAGED, 
+                processor_count * sizeof(VMM_STATE), 
+                POOL_TAG_VMM);
+
+        if (!vmm_state)
+                return FALSE;
+
+        for (ULONG index = 0; index < processor_count; index++)
+        {
+                KeSetSystemAffinityThread(1ull << index);
+                hvdbgEnableVmxOperationOnCore();
+
+                DEBUG_LOG("VMX Operation enable on core: %lx", index);
+
+                hvdbgAllocateVmcsRegion(index);
+                hvdbgAllocateVmxonRegion(index);
         }
 
         return TRUE;
--- hv-dbg/driver.h
+++ hv-dbg/driver.h
@@ -3,14 +3,19 @@
 
 #include <ntifs.h>
 
+#define DEBUG_LOG(fmt, ...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "[+] " fmt "\n", ##__VA_ARGS__)
+#define DEBUG_ERROR(fmt, ...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "[-] " fmt "\n", ##__VA_ARGS__)
+
 #define STATIC static
+
+#define POOL_TAG_VMM 'vmmm'
 
 typedef struct _CPUID
 {
-    INT eax;
-    INT ebx;
-    INT ecx;
-    INT edx;
+        INT eax;
+        INT ebx;
+        INT ecx;
+        INT edx;
 
 } CPUID, * PCPUID;
 
@@ -47,23 +52,87 @@ typedef struct _CPUID
 #define MSR_GS_BASE        0xC0000101
 #define MSR_SHADOW_GS_BASE 0xC0000102 // SwapGS GS shadow
 
+#define ALIGNMENT_PAGE_SIZE 4096
+#define MAXIMUM_ADDRESS     0xffffffffffffffff
+#define VMCS_SIZE           4096
+#define VMXON_SIZE          4096
+
 typedef union _IA32_FEATURE_CONTROL_MSR
 {
-    ULONG64 All;
+        struct
+        {
+                ULONG64 Lock : 1;               // [0]
+                ULONG64 EnableSMX : 1;          // [1]
+                ULONG64 EnableVmxon : 1;        // [2]
+                ULONG64 Reserved2 : 5;          // [3-7]
+                ULONG64 EnableLocalSENTER : 7;  // [8-14]
+                ULONG64 EnableGlobalSENTER : 1; // [15]
+                ULONG64 Reserved3a : 16;        //
+                ULONG64 Reserved3b : 32;        // [16-63]
 
-    struct
-    {
-        ULONG64 Lock : 1;               // [0]
-        ULONG64 EnableSMX : 1;          // [1]
-        ULONG64 EnableVmxon : 1;        // [2]
-        ULONG64 Reserved2 : 5;          // [3-7]
-        ULONG64 EnableLocalSENTER : 7;  // [8-14]
-        ULONG64 EnableGlobalSENTER : 1; // [15]
-        ULONG64 Reserved3a : 16;        //
-        ULONG64 Reserved3b : 32;        // [16-63]
-
-    } bits;
+        } bits;
+	UINT64 bit_address;
 
 } IA32_FEATURE_CONTROL_MSR, * PIA32_FEATURE_CONTROL_MSR;
+
+typedef union _CR4
+{
+	struct
+	{
+		UINT64 vme : 1;
+		UINT64 pvi : 1;
+		UINT64 tsd : 1;
+		UINT64 de : 1;
+		UINT64 pse : 1;
+		UINT64 pae : 1;
+		UINT64 mce : 1;
+		UINT64 pge : 1;
+		UINT64 pce : 1;
+		UINT64 osfxsr : 1;
+		UINT64 osxmmexcpt : 1;
+		UINT64 umip : 1;
+		UINT64 la57 : 1;
+		UINT64 vmxe : 1;
+		UINT64 smxe : 1;
+		UINT64 reserved_1 : 1;
+		UINT64 fsgsbase : 1;
+		UINT64 pcide : 1;
+		UINT64 osxsave : 1;
+		UINT64 kl : 1;
+		UINT64 smep : 1;
+		UINT64 smap : 1;
+		UINT64 pke : 1;
+		UINT64 cet : 1;
+		UINT64 pks : 1;
+		UINT64 uintr : 1;
+		UINT64 reserved_2 : 37;
+
+	} bits;
+
+	UINT64 bit_address;
+
+} CR4, * PCR4;
+
+typedef union _IA32_VMX_BASIC_MSR
+{
+	struct
+	{
+		ULONG32 RevisionIdentifier : 31;  // [0-30]
+		ULONG32 Reserved1 : 1;            // [31]
+		ULONG32 RegionSize : 12;          // [32-43]
+		ULONG32 RegionClear : 1;          // [44]
+		ULONG32 Reserved2 : 3;            // [45-47]
+		ULONG32 SupportedIA64 : 1;        // [48]
+		ULONG32 SupportedDualMoniter : 1; // [49]
+		ULONG32 MemoryType : 4;           // [50-53]
+		ULONG32 VmExitReport : 1;         // [54]
+		ULONG32 VmxCapabilityHint : 1;    // [55]
+		ULONG32 Reserved3 : 8;            // [56-63]
+
+	} bits;
+
+	UINT64 bit_address;
+
+} IA32_VMX_BASIC_MSR, * PIA32_VMX_BASIC_MSR;
 
 #endif
