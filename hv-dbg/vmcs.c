c9bf071b8980869670f34e4b8a0c2aaf8fdf4b55
lhodges1 <lachiehodges7785@gmail.com>
2023-10-15 17:09:11 +1100

    control fields state

--- hv-dbg/vmcs.c
+++ hv-dbg/vmcs.c
@@ -69,88 +69,9 @@ EncodeField(
 	return encoding.address;
 }
 
-typedef struct _NATURAL_STATE
-{
-	UINT64 cr0;
-	UINT64 cr3;
-	UINT64 cr4;
-	UINT64 es_base;
-	UINT64 cs_base;
-	UINT64 ss_base;
-	UINT64 ds_base;
-	UINT64 fs_base;
-	UINT64 gs_base;
-	UINT64 ldtr_base;
-	UINT64 tr_base;
-	UINT64 gdtr_base;
-	UINT64 idtr_base;
-	UINT64 dr7;
-	UINT64 rsp;
-	UINT64 rip;
-	UINT64 rflags;
-	UINT64 sysenter_esp;
-	UINT64 sysenter_eip;
-
-}NATURAL_STATE, *PNATURAL_STATE;
-typedef struct _QWORD_BIT_STATE
-{
-	UINT64 vmcs_link_pointer;
-	UINT64 debug_control;
-	UINT64 pat;
-	UINT64 efer;
-	UINT64 perf_global_control;
-	UINT64 bndcfgs;
-
-}QWORD_BIT_STATE, *PQWORD_BIT_STATE;
-typedef struct _DWORD_BIT_STATE
-{
-	UINT32 es_limit;
-	UINT32 cs_limit;
-	UINT32 ss_limit;
-	UINT32 ds_limit;
-	UINT32 fs_limit;
-	UINT32 gs_limit;
-	UINT32 ldtr_limit;
-	UINT32 tr_limit;
-	UINT32 gdtr_limit;
-	UINT32 idtr_limit;
-	UINT32 es_access_rights;
-	UINT32 cs_access_rights;
-	UINT32 ss_access_rights;
-	UINT32 ds_access_rights;
-	UINT32 fs_access_rights;
-	UINT32 gs_access_rights;
-	UINT32 ldtr_access_rights;
-	UINT32 tr_access_rights;
-	UINT32 smbase;
-	UINT32 sysenter_cs;
-
-}DWORD_BIT_STATE, *PDWORD_BIT_STATE;
-typedef struct _WORD_BIT_STATE
-{
-	UINT16 es_selector;
-	UINT16 cs_selector;
-	UINT16 ss_selector;
-	UINT16 ds_selector;
-	UINT16 fs_selector;
-	UINT16 gs_selector;
-	UINT16 ldtr_selector;
-	UINT16 tr_selector;
-
-}WORD_BIT_STATE, *PWORD_BIT_STATE;
-typedef struct _VMCS_GUEST_STATE_FIELDS
-{
-	NATURAL_STATE natural_state;
-	QWORD_BIT_STATE qword_state;
-	DWORD_BIT_STATE dword_state;
-	WORD_BIT_STATE word_state;
-
-
-}VMCS_GUEST_STATE_FIELDS, *PVMCS_GUEST_STATE_FIELDS;
-
 STATIC
 VOID
-EncodeVmcsStateFields(
+EncodeVmcsGuestStateFields(
 	_Out_ PVMCS_GUEST_STATE_FIELDS Fields
 )
 {
@@ -223,6 +144,124 @@ EncodeVmcsStateFields(
 	Fields->word_state.tr_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_GUEST_STATE, VMCS_WIDTH_16, 7);
 }
 
+STATIC
+VOID
+EncodeVmcsHostStateFields(
+	_Out_ PVMCS_HOST_STATE_FIELDS Fields
+)
+{
+	if (!Fields)
+		return;
+
+	/* natural */
+
+	Fields->natural_state.cr0 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 0);
+	Fields->natural_state.cr3 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 1);
+	Fields->natural_state.cr4 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 2);
+	Fields->natural_state.rsp = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 3);
+	Fields->natural_state.rip = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 4);
+	Fields->natural_state.fs_base = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 5);
+	Fields->natural_state.gs_base = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 6);
+	Fields->natural_state.tr_base = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 7);
+	Fields->natural_state.gdtr_base = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 8);
+	Fields->natural_state.idtr_base = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 9);
+	Fields->natural_state.ia32_sysenter_esp = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 10);
+	Fields->natural_state.ia32_sysenter_eip = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 11);
+	Fields->natural_state.ia32_perf_global_ctrl = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_64, 12);
+
+	/* 16 bit */
+
+	Fields->word_state.es_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 0);
+	Fields->word_state.cs_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 1);
+	Fields->word_state.ss_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 2);
+	Fields->word_state.ds_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 3);
+	Fields->word_state.fs_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 4);
+	Fields->word_state.gs_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 5);
+	Fields->word_state.tr_selector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_16, 6);
+
+	/* 32 bit */
+
+	Fields->dword_state.ia32_sysenter_cs = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_HOST_STATE, VMCS_WIDTH_32, 0);
+}
+
+STATIC
+VOID
+EncodeVmcsControlStateFields(
+	_In_ PVMCS_CONTROL_STATE_FIELDS Fields
+)
+{
+	if (!Fields)
+		return;
+
+	/* natural state */
+	Fields->natural_state.cr0_guest_host_mask = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 0);
+	Fields->natural_state.cr4_guest_host_mask = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 1);
+	Fields->natural_state.cr0_read_shadow = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 2);
+	Fields->natural_state.cr4_read_shadow = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 3);
+	Fields->natural_state.cr3_target_value_0 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 4);
+	Fields->natural_state.cr3_target_value_1 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 5);
+	Fields->natural_state.cr3_target_value_2 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 6);
+	Fields->natural_state.cr3_target_value_3 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_NATURAL, 7);
+
+	/* 64bit state */
+
+	Fields->qword_state.io_bitmap_a_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 0);
+	Fields->qword_state.io_bitmap_b_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 1);
+	Fields->qword_state.msr_bitmap_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 2);
+	Fields->qword_state.vmexit_msr_store_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 3);
+	Fields->qword_state.vmexit_msr_load_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 4);
+	Fields->qword_state.vmentry_msr_load_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 5);
+	Fields->qword_state.executive_vmcs_pointer = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 6);
+	Fields->qword_state.pml_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 7);
+	Fields->qword_state.tsc_offset = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 8);
+	Fields->qword_state.virtual_apic_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 9);
+	Fields->qword_state.apic_access_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 10);
+	Fields->qword_state.posted_interrupt_descriptor_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 11);
+	Fields->qword_state.vmfunc_controls = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 12);
+	Fields->qword_state.ept_pointer = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 13);
+	Fields->qword_state.eoi_exit_bitmap_0 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 14);
+	Fields->qword_state.eoi_exit_bitmap_1 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 15);
+	Fields->qword_state.eoi_exit_bitmap_2 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 16);
+	Fields->qword_state.eoi_exit_bitmap_3 = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 17);
+	Fields->qword_state.ept_pointer_list_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 18);
+	Fields->qword_state.vmread_bitmap_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 19);
+	Fields->qword_state.vmwrite_bitmap_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 20);
+	Fields->qword_state.virtualization_exception_info_address = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 21);
+	Fields->qword_state.xss_exiting_bitmap = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 22);
+	Fields->qword_state.encls_exiting_bitmap = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 23);
+	Fields->qword_state.tsc_multiplier = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_64, 25);
+
+	/* 32 bit state */
+
+	Fields->dword_state.pin_based_vm_execution_controls = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 0);
+	Fields->dword_state.processor_based_vm_execution_controls = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 1);
+	Fields->dword_state.exception_bitmap = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 2);
+	Fields->dword_state.pagefault_error_code_mask = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 3);
+	Fields->dword_state.pagefault_error_code_match = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 4);
+	Fields->dword_state.cr3_target_count = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 5);
+	Fields->dword_state.vmexit_controls = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 6);
+	Fields->dword_state.vmexit_msr_store_count = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 7);
+	Fields->dword_state.vmexit_msr_load_count = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 8);
+	Fields->dword_state.vmentry_controls = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 9);
+	Fields->dword_state.vmentry_msr_load_count = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 10);
+	Fields->dword_state.vmentry_interruption_info = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 11);
+	Fields->dword_state.vmentry_exception_error_code = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 12);
+	Fields->dword_state.vmentry_instruction_length = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 13);
+	Fields->dword_state.tpr_threshold = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 14);
+	Fields->dword_state.secondary_processor_based_vm_execution_controls = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 15);
+	Fields->dword_state.ple_gap = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 16);
+	Fields->dword_state.ple_window = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_32, 17);
+
+	/* 16 bit state */
+
+	Fields->word_state.virtual_processor_identifier = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_16, 0);
+	Fields->word_state.posted_interrupt_notification_vector = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_16, 1);
+	Fields->word_state.eptp_index = EncodeField(VMCS_ACCESS_FULL, VMCS_TYPE_CONTROL, VMCS_WIDTH_16, 2);
+
+
+
+}
+
 NTSTATUS
 InitiateVmcsRegion(
 	_Inout_ PVMM_STATE VmmState
@@ -230,9 +269,12 @@ InitiateVmcsRegion(
 {
 	NTSTATUS status = STATUS_SUCCESS;
 	VMCS_GUEST_STATE_FIELDS guest_fields = { 0 };
+	VMCS_HOST_STATE_FIELDS host_fields = { 0 };
+	VMCS_CONTROL_STATE_FIELDS control_fields = { 0 };
 
-	/* encode our guest_fields */
-	EncodeVmcsStateFields(&guest_fields);
+	EncodeVmcsGuestStateFields(&guest_fields);
+	EncodeVmcsGuestStateFields(&host_fields);
+	EncodeVmcsGuestStateFields(&control_fields);
 
 	if (__vmx_vmclear(&VmmState->vmcs_region_pa) != VMX_OK || 
 		__vmx_vmptrld(&VmmState->vmcs_region_pa))
@@ -268,4 +310,28 @@ InitiateVmcsRegion(
 	__vmx_vmwrite(guest_fields.dword_state.ldtr_limit, __segmentlimit(__read_ldtr()));
 	__vmx_vmwrite(guest_fields.dword_state.ss_limit, __segmentlimit(__read_ss()));
 	__vmx_vmwrite(guest_fields.dword_state.tr_limit, __segmentlimit(__read_tr()));
+
+	__vmx_vmwrite(guest_fields.qword_state.vmcs_link_pointer, ~0ull);
+
+	__vmx_vmwrite(guest_fields.qword_state.debug_control, __readmsr(MSR_IA32_DEBUGCTL) & 0xffffffff);
+
+	__vmx_vmwrite(host_fields.natural_state.cr0, __readcr0());
+	__vmx_vmwrite(host_fields.natural_state.cr3, __readcr3());
+	__vmx_vmwrite(host_fields.natural_state.cr4, __readcr4());
+
+	__vmx_vmwrite(host_fields.word_state.cs_selector, __read_cs() & 0xf8);
+	__vmx_vmwrite(host_fields.word_state.ds_selector, __read_ds() & 0xf8);
+	__vmx_vmwrite(host_fields.word_state.es_selector, __read_es() & 0xf8);
+	__vmx_vmwrite(host_fields.word_state.fs_selector, __read_fs() & 0xf8);
+	__vmx_vmwrite(host_fields.word_state.gs_selector, __read_gs() & 0xf8);
+	__vmx_vmwrite(host_fields.word_state.ss_selector, __read_ss() & 0xf8);
+	__vmx_vmwrite(host_fields.word_state.tr_selector, __read_tr() & 0xf8);
+
+	__vmx_vmwrite(control_fields.qword_state.tsc_offset, 0);
+	__vmx_vmwrite(control_fields.dword_state.pagefault_error_code_mask, 0);
+	__vmx_vmwrite(control_fields.dword_state.pagefault_error_code_match, 0);
+	__vmx_vmwrite(control_fields.dword_state.vmexit_msr_load_count, 0);
+	__vmx_vmwrite(control_fields.dword_state.vmexit_msr_store_count, 0);
+	__vmx_vmwrite(control_fields.dword_state.vmentry_msr_load_count, 0);
+	__vmx_vmwrite(control_fields.dword_state.vmentry_interruption_info, 0);
 }
--- hv-dbg/vmcs.h
+++ hv-dbg/vmcs.h
@@ -3,4 +3,217 @@
 
 #include <ntifs.h>
 
+typedef struct _NATURAL_STATE
+{
+	UINT64 cr0;
+	UINT64 cr3;
+	UINT64 cr4;
+	UINT64 es_base;
+	UINT64 cs_base;
+	UINT64 ss_base;
+	UINT64 ds_base;
+	UINT64 fs_base;
+	UINT64 gs_base;
+	UINT64 ldtr_base;
+	UINT64 tr_base;
+	UINT64 gdtr_base;
+	UINT64 idtr_base;
+	UINT64 dr7;
+	UINT64 rsp;
+	UINT64 rip;
+	UINT64 rflags;
+	UINT64 sysenter_esp;
+	UINT64 sysenter_eip;
+
+}NATURAL_STATE, * PNATURAL_STATE;
+
+typedef struct _QWORD_BIT_STATE
+{
+	UINT64 vmcs_link_pointer;
+	UINT64 debug_control;
+	UINT64 pat;
+	UINT64 efer;
+	UINT64 perf_global_control;
+	UINT64 bndcfgs;
+
+}QWORD_BIT_STATE, * PQWORD_BIT_STATE;
+
+typedef struct _DWORD_BIT_STATE
+{
+	UINT32 es_limit;
+	UINT32 cs_limit;
+	UINT32 ss_limit;
+	UINT32 ds_limit;
+	UINT32 fs_limit;
+	UINT32 gs_limit;
+	UINT32 ldtr_limit;
+	UINT32 tr_limit;
+	UINT32 gdtr_limit;
+	UINT32 idtr_limit;
+	UINT32 es_access_rights;
+	UINT32 cs_access_rights;
+	UINT32 ss_access_rights;
+	UINT32 ds_access_rights;
+	UINT32 fs_access_rights;
+	UINT32 gs_access_rights;
+	UINT32 ldtr_access_rights;
+	UINT32 tr_access_rights;
+	UINT32 smbase;
+	UINT32 sysenter_cs;
+
+}DWORD_BIT_STATE, * PDWORD_BIT_STATE;
+
+typedef struct _WORD_BIT_STATE
+{
+	UINT16 es_selector;
+	UINT16 cs_selector;
+	UINT16 ss_selector;
+	UINT16 ds_selector;
+	UINT16 fs_selector;
+	UINT16 gs_selector;
+	UINT16 ldtr_selector;
+	UINT16 tr_selector;
+
+}WORD_BIT_STATE, * PWORD_BIT_STATE;
+
+typedef struct _VMCS_GUEST_STATE_FIELDS
+{
+	NATURAL_STATE natural_state;
+	QWORD_BIT_STATE qword_state;
+	DWORD_BIT_STATE dword_state;
+	WORD_BIT_STATE word_state;
+
+
+}VMCS_GUEST_STATE_FIELDS, * PVMCS_GUEST_STATE_FIELDS;
+
+/*
+* Host state area
+*/
+typedef struct _HOST_WORD_BIT_STATE
+{
+	UINT16 es_selector;
+	UINT16 cs_selector;
+	UINT16 ss_selector;
+	UINT16 ds_selector;
+	UINT16 fs_selector;
+	UINT16 gs_selector;
+	UINT16 tr_selector;
+
+}HOST_WORD_BIT_STATE, * PHOST_WORD_BIT_STATE;
+
+typedef struct _HOST_NATURAL_BIT_STATE
+{
+	UINT64 cr0;
+	UINT64 cr3;
+	UINT64 cr4;
+	UINT64 rsp;
+	UINT64 rip;
+	UINT64 fs_base;
+	UINT64 gs_base;
+	UINT64 tr_base;
+	UINT64 gdtr_base;
+	UINT64 idtr_base;
+	UINT64 ia32_sysenter_esp;
+	UINT64 ia32_sysenter_eip;
+	UINT64 ia32_perf_global_ctrl;
+
+}HOST_NATURAL_BIT_STATE, *PHOST_NATURAL_BIT_STATE;
+
+typedef struct _HOST_DWORD_BIT_STATE
+{
+	UINT32 ia32_sysenter_cs;
+
+}HOST_DWORD_BIT_STATE, *PHOST_DWORD_BIT_STATE;
+
+typedef struct _VMCS_HOST_STATE_FIELDS
+{
+	HOST_WORD_BIT_STATE word_state;
+	HOST_DWORD_BIT_STATE dword_state;
+	HOST_NATURAL_BIT_STATE natural_state;
+
+}VMCS_HOST_STATE_FIELDS, *PVMCS_HOST_STATE_FIELDS;
+
+typedef struct _CONTROL_QWORD_BIT_STATE
+{
+	UINT64 io_bitmap_a_address;
+	UINT64 io_bitmap_b_address;
+	UINT64 msr_bitmap_address;
+	UINT64 vmexit_msr_store_address;
+	UINT64 vmexit_msr_load_address;
+	UINT64 vmentry_msr_load_address;
+	UINT64 executive_vmcs_pointer;
+	UINT64 pml_address;
+	UINT64 tsc_offset;
+	UINT64 virtual_apic_address;
+	UINT64 apic_access_address;
+	UINT64 posted_interrupt_descriptor_address;
+	UINT64 vmfunc_controls;
+	UINT64 ept_pointer;
+	UINT64 eoi_exit_bitmap_0;
+	UINT64 eoi_exit_bitmap_1;
+	UINT64 eoi_exit_bitmap_2;
+	UINT64 eoi_exit_bitmap_3;
+	UINT64 ept_pointer_list_address;
+	UINT64 vmread_bitmap_address;
+	UINT64 vmwrite_bitmap_address;
+	UINT64 virtualization_exception_info_address;
+	UINT64 xss_exiting_bitmap;
+	UINT64 encls_exiting_bitmap;
+	UINT64 tsc_multiplier;
+
+} CONTROL_QWORD_BIT_STATE, * PCONTROL_QWORD_BIT_STATE;
+
+typedef struct _CONTROL_DWORD_BIT_STATE
+{
+	UINT32 pin_based_vm_execution_controls;
+	UINT32 processor_based_vm_execution_controls;
+	UINT32 exception_bitmap;
+	UINT32 pagefault_error_code_mask;
+	UINT32 pagefault_error_code_match;
+	UINT32 cr3_target_count;
+	UINT32 vmexit_controls;
+	UINT32 vmexit_msr_store_count;
+	UINT32 vmexit_msr_load_count;
+	UINT32 vmentry_controls;
+	UINT32 vmentry_msr_load_count;
+	UINT32 vmentry_interruption_info;
+	UINT32 vmentry_exception_error_code;
+	UINT32 vmentry_instruction_length;
+	UINT32 tpr_threshold;
+	UINT32 secondary_processor_based_vm_execution_controls;
+	UINT32 ple_gap;
+	UINT32 ple_window;
+
+} CONTROL_DWORD_BIT_STATE, *PCONTROL_DWORD_BIT_STATE;
+
+typedef struct _CONTROL_WORD_BIT_STATE
+{
+	UINT16 virtual_processor_identifier;
+	UINT16 posted_interrupt_notification_vector;
+	UINT16 eptp_index;
+
+} CONTROL_WORD_BIT_STATE, * PCONTROL_WORD_BIT_STATE;
+
+typedef struct _CONTROL_NATURAL_BIT_STATE
+{
+	UINT64 cr0_guest_host_mask;
+	UINT64 cr4_guest_host_mask;
+	UINT64 cr0_read_shadow;
+	UINT64 cr4_read_shadow;
+	UINT64 cr3_target_value_0;
+	UINT64 cr3_target_value_1;
+	UINT64 cr3_target_value_2;
+	UINT64 cr3_target_value_3;
+
+} CONTROL_NATURAL_BIT_STATE, *PCONTROL_NATURAL_BIT_STATE;
+
+typedef struct _VMCS_CONTROL_STATE_FIELDS
+{
+	CONTROL_WORD_BIT_STATE word_state;
+	CONTROL_DWORD_BIT_STATE dword_state;
+	CONTROL_QWORD_BIT_STATE qword_state;
+	CONTROL_NATURAL_BIT_STATE natural_state;
+
+}VMCS_CONTROL_STATE_FIELDS, *PVMCS_CONTROL_STATE_FIELDS;
+
 #endif
